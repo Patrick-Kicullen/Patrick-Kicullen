45bb7d674247c758dcb91ce49629190e577317de
psypk6 <psypk6@nottingham.ac.uk>
2022-11-09 14:28:29 +0000

    Added testimonial

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,20 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+$(function() {
+  $('.owl-carousel.testimonial-carousel').owlCarousel({
+    nav: true,
+    navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      750: {
+        items: 2,
+      }
+    }
+  });
+});
